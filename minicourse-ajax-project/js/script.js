e043900ac707b2e046b6cea715eb86176640e54e
Trish Whetzel <plwhetzel@gmail.com>
2016-12-08 00:29:47 -0500

    add API call for New York Times

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -12,25 +12,44 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var streetName = $('#street').val();
+    var cityName = $('#city').val();
 
-
-    // YOUR CODE GOES HERE!
-    var street_name = $('#street').val();
-
-    var city_name = $('#city').val();
-    console.log(city_name);
-
-    var address = street_name + ', ' + city_name;
+    var address = streetName + ', ' + cityName;
 
     $greeting.text('So you want to live at ' + address + '?');
 
-    var streetview_url = "https://maps.googleapis.com/maps/api/streetview?";
+    // Build Google Streetview Web service call
+    var streetviewUrl = "https://maps.googleapis.com/maps/api/streetview";
     var size = "size=600x400";
-    var api_key = "&key=AIzaSyBSt5zMFbNasn8gKdGIhwaJ5ny6vw7S9ug";
-    var location = "&location="+address
-    var streetview_ws_call = streetview_url+size+location+api_key
-    console.log(streetview_ws_call)
-    $body.append('<img class="bgimg" src="' + streetview_ws_call + '">');
+    var streetviewApiKey = "&key=AIzaSyBSt5zMFbNasn8gKdGIhwaJ5ny6vw7S9ug";
+    var location = "&location="+ address
+    streetviewUrl += '?' + size + location + streetviewApiKey;
+    console.log(streetviewUrl)
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    // Build New York Times Web service call
+    var nytimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + streetName + "&sort=newest";
+    var nytApiKey = "c0455864d47141d29259369461fa578c";
+
+    nytimesUrl += '&' + $.param({
+        'api-key': nytApiKey
+    });
+
+    $.getJSON( nytimesUrl, function( data ) {
+        $nytHeaderElem.text('New York Times articles about ' + address);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            console.log(article)
+            $('#form-container').submit(loadData);
+            $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+        };
+    });
+
+
     return false;
 };
 
